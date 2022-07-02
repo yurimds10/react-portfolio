465733b4238a6770282aae07de303393faf89ef9
YuriMendess <contato.yurimendess@gmail.com>
2022-07-01 22:50:58 -0300

    remove - globalstyle and scroll down button

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,13 @@ import Header from './Components/Header/Index';
 import Home from './Components/Home/Index';
 import About from './Components/About/Index';
 import Skills from './Components/Skills/Index';
-import Services from './Components/Services-/Index';
 import Portfolio from './Components/Portfolio/Index';
 import Contact from './Components/Contact/Index';
 import Footer from './Components/Footer/Index';
-import ScrollUpButton from './Components/ScrollUp/Index';
-import { GlobalStyle } from './Styles/GlobalStyle';
 
 export default function App():JSX.Element {
   return (
     <div>
-      <GlobalStyle/>
       <Header/>
       <main>
         <Home/>
